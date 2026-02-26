b1a4a520ce5e439e406b48d58017c27bdcd473cd
Shubham Sharma <sharmashibu787@gmail.com>
2026-02-26 21:21:14 +0530

    chnage grid section food card with vanillla css

--- src/app/components/FoodCard/FoodCard.jsx
+++ src/app/components/FoodCard/FoodCard.jsx
@@ -1,12 +1,26 @@
 import Image from "next/image";
+
 export default function FoodCard({ title, price, image }) {
   return (
-    <div className="card">
-      <Image src={image} alt={title} width={300} height={200} />
-      <div className="card-body">
-        <h3>{title}</h3>
-        <p className="price">₹{price}</p>
-        <button className="btn">Order Now</button>
+    <div className="card-item">
+      <div className="card">
+        <div className="card-image">
+          <Image
+            src={image}
+            alt={title}
+            fill
+            sizes="(max-width: 768px) 100vw,
+                   (max-width: 1200px) 50vw,
+                   33vw"
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+
+        <div className="card-body">
+          <h3 className="card-title">{title}</h3>
+          <p className="price">₹{price}</p>
+          <button className="btn order-btn">Order Now</button>
+        </div>
       </div>
     </div>
   );
